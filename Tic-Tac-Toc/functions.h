090b2e95ab346bd5cfdbf882eb227209fd1cd9c4
Mohamed Emad Mahrous <123952213+3omdaughh@users.noreply.github.com>
2023-12-23 13:58:04 +0200

    Add files via upload

---
+++ Tic-Tac-Toc/functions.h
@@ -0,0 +1,13 @@
+/*
+ *
+ *
+ */
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+
+void displayBoard();
+bool isWinner(char player);
+bool isFull();
+void playerMove(char player);
